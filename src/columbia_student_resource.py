beb1cdf7e3a9def8e11b4256228843f5c54d8e00
zipeijiang <60901774+zipeijiang@users.noreply.github.com>
2022-09-27 12:57:38 -0400

    configure env var

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -16,9 +16,9 @@ class ColumbiaStudentResource:
         h = os.environ.get("DBHOST")
 
         conn = pymysql.connect(
-            user=usr,
-            password=pw,
-            host=h,
+            user='admin',
+            password='12345678',
+            host='e6156hw0.cmn7wrhtp6ci.us-east-1.rds.amazonaws.com',
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
